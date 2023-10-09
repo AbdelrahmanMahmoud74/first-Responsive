6b16faf7647946eb23079086b62ccfe408bcf801
AbdelrahmanMahmoud74 <133714220+AbdelrahmanMahmoud74@users.noreply.github.com>
2023-10-09 16:07:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var allproducts=document.querySelectorAll(".list li")
+var div=document.querySelector("#content")
+var btn=document.querySelector("#btn")
+var money=document.querySelector("#money")
+var totalprice=0
+allproducts.forEach(function(item){
+    item.onclick=function(){
+        div.innerHTML+=item.textContent+ "+"
+        totalprice+= +(item.getAttribute("price"))
+        if(div.innerHTML!=""){
+            btn.style.display="block";
+            btn.style.width="150px";
+            btn.style.height="40px";
+            btn.style.backgroundColor="blue";
+            btn.style.color="white";
+            btn.style.borderRadious="5px";
+        }
+    }
+})
+btn.onclick=function(){
+
+    money.innerHTML="total:"+totalprice
+}
